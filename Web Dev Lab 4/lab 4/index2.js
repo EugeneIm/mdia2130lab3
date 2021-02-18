210039391fd0aa04dbfa1f74f073f7bdc0b6701d
EugeneIm <immer277@gmail.com>
2021-02-17 19:18:18 -0800

    commit

---
+++ Web Dev Lab 4/lab 4/index2.js
@@ -0,0 +1,84 @@
+
+window.onload = doStuff // when the window is finished loading, call the doStuff function
+function doStuff()
+{
+let textbox = document.getElementById("t")
+textbox.onkeyup = function()
+{
+    document.getElementById('para').innerHeight = this.value
+}
+
+textbox.onclick = function()
+{
+    if(this.value.toLowerCase() == "dog")
+    {
+        document.getElementById('dog').style.border = "5px red dashed"
+    }
+    else
+    {
+        document.getElementById('dog').style.border = ""
+    }
+    if(this.value.toLowerCase() == "bird")
+    {
+        document.getElementById('bird').style.border = "5px red dashed"
+    }
+    else
+    {
+        document.getElementById('bird').style.border = ""
+    }
+    if(this.value.toLowerCase() == "cat")
+    {
+        document.getElementById('cat').style.border = "5px red dashed"
+    }
+    else 
+    {
+        document.getElementById('cat').style.border = ""
+    }
+    if(this.value.toLowerCase() == "fish")
+    {
+        document.getElementById('fish').style.border = "5px red dashed"
+    }
+    else
+    {
+        document.getElementById('fish').style.border = ""
+    }
+    if(this.value.toLowerCase() == "turtle")
+    {
+        document.getElementById('turtle').style.border = "5px red dashed"
+    }
+    else
+    {
+        document.getElementById('turtle').style.border = ""
+    }
+    if(this.value.toLowerCase() == "all")
+    {
+        paras = document.getElementsByTagName('img');
+        for(let i = 0; i < paras.length; i++)
+        {
+            paras[i].style.border = "3px red dashed"
+        }
+    }
+    if(this.value.toLowerCase() == "")
+    {
+        alert("missing data")
+        document.getElementById('t').style.backgroundColor = "yellow"
+    }
+    if(this.value.toLowerCase() == "water")
+    {
+        document.getElementById('fish').style.border = "3px red dashed"
+        document.getElementById('turtle').style.border = "3px red dashed"
+    }
+    
+}
+
+
+
+var img = document.getElementsByTagName('img');
+for(i = 0;i < img.length; i++)
+{
+    if(img.length >= 3)
+    {
+        img[i].style.width = "200px"
+    }
+}
+}
